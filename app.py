853d49a69329e1eb01e139c3c2c5f42f21b72ab0
Venkateswarlu Allacheruvu <vallacheruvu@gmail.com>
2022-07-06 14:36:28 +0530

    testing ver congtrol

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restful import Api
 from flask_jwt import JWT
 from db import db
 
+print("Ven")
+
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.predict import Prediction
